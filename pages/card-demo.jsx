cfb4622074668143e0660b8a35e14719605e85af
= <57877722+bn-s@users.noreply.github.com>
2022-03-09 15:34:50 +0100

    Add rotate suit/value buttons to card demo

--- pages/card-demo.jsx
+++ pages/card-demo.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
+
 import Head from 'next/head'
 
 import PlayingCard from '../components/PlayingCards/PlayingCard'
 
 export default function Home() {
+  const [value, setValue] = useState(0)
+  const [suit, setSuit] = useState(0)
   return (
     <>
       <Head>
@@ -14,7 +18,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col items-center justify-center w-full h-screen">
-        <PlayingCard value={5} suit="K" />
+        <div>
+          <button
+            type="button"
+            className="px-6 py-4 m-2 bg-red-500"
+            onClick={() => setValue((value + 1) % 13)}
+          >
+            rotate value
+          </button>
+          <button
+            type="button"
+            className="px-6 py-4 m-2 bg-green-500"
+            onClick={() => setSuit((suit + 1) % 4)}
+          >
+            rotate suit
+          </button>
+        </div>
+        <PlayingCard value={value} suit={['h', 'c', 'd', 's'][suit]} />
       </div>
     </>
   )
